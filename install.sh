4e69d2f3eda2eb48dfaa6f35e49db41f6e4b4f81
Johan Selberg <johan.selberg@gmail.com>
2019-05-31 00:03:46 +0200

    Give the unicode proper variable names.

--- install.sh
+++ install.sh
@@ -18,10 +18,10 @@ readonly __script_dir=$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
 readonly __dotfiles_backup_dir="${HOME}/dotfiles.bak"
 
 # unicode symbols
-readonly __head_action="\u226B"
-readonly __add_action="\u2300"
-readonly __action_step="\u21AA"
-readonly __action_completed="\u2713"
+readonly __job_symbol="\u226B"
+readonly __job_task_symbol="\u2300"
+readonly __job_task_step_symbol="\u21AA"
+readonly __job_completed="\u2713"
 readonly __info_message="\u2022"
 readonly __seperator="\u2501"
 
@@ -47,7 +47,7 @@ print_seperator() {
 
 print_info() {
     print_seperator
-    printf "${__head_action} Info\n"
+    printf "${__job_symbol} Info\n"
     print_seperator
     printf "${__info_message} This is an installation script for installing my dotfiles on a system.\n"
     printf "${__info_message} The dotfiles will be stored in the directory which you're running\n"
@@ -61,24 +61,24 @@ print_info() {
 
 input_prompt() {
     while true; do
-        printf "${__add_action} "
+        printf "${__job_task_symbol} "
         read -p "Do you still wish to run the script? " response
         case "${response}" in
-            [Yy]* ) printf "  ${__action_step} Proceeding with the installation! \\o/\n"; break;;
-            [Nn]* ) printf "  ${__action_step} bye :(\n"; exit;;
+            [Yy]* ) printf "  ${__job_task_step_symbol} Proceeding with the installation! \\o/\n"; break;;
+            [Nn]* ) printf "  ${__job_task_step_symbol} bye :(\n"; exit;;
         esac
     done
 }
 
 check_if_home_is_set() {
-    printf "${__add_action} Checking if \$HOME is set\n"
+    printf "${__job_task_symbol} Checking if \$HOME is set\n"
     if [ -z "${HOME}" ]; then
-        printf "  ${__action_step} You got no \$HOME :(\n"
-        printf "    ${__action_step} \$HOME has to be set to your home directory for the script\n" 
+        printf "  ${__job_task_step_symbol} You got no \$HOME :(\n"
+        printf "    ${__job_task_step_symbol} \$HOME has to be set to your home directory for the script\n" 
         printf "      to be able to run properly\n"
         exit 1
     else
-        printf "  ${__action_step} You got a \$HOME \\o/\n"
+        printf "  ${__job_task_step_symbol} You got a \$HOME \\o/\n"
     fi
     print_seperator
 }
@@ -86,58 +86,58 @@ check_if_home_is_set() {
 backup_dotfile() {
     local dotfile_path="${1}"
 
-    printf "  ${__action_step} Backing up %s\n" $(basename "${dotfile_path}")
+    printf "  ${__job_task_step_symbol} Backing up %s\n" $(basename "${dotfile_path}")
 
     if [ -f "${dotfile_path}" ] || [ -d "${dotfile_path}" ]; then
-        printf "    ${__action_step} %s exists on the system\n" $(basename "${dotfile_path}")
-        printf "      ${__action_step} Copying %s to %s\n" $(basename "${dotfile_path}") "${__dotfiles_backup_dir}" 
+        printf "    ${__job_task_step_symbol} %s exists on the system\n" $(basename "${dotfile_path}")
+        printf "      ${__job_task_step_symbol} Copying %s to %s\n" $(basename "${dotfile_path}") "${__dotfiles_backup_dir}" 
         cp -r "${dotfile_path}" "${__dotfiles_backup_dir}"
-        printf "      ${__action_step} Removing old %s from the system\n" $(basename "${dotfile_path}")
+        printf "      ${__job_task_step_symbol} Removing old %s from the system\n" $(basename "${dotfile_path}")
         rm -rf "${dotfile_path}"
     else
-        printf "    ${__action_step} %s doesn't exist\n" $(basename "${dotfile_path}")
-        printf "      ${__action_step} No need to backup\n"
+        printf "    ${__job_task_step_symbol} %s doesn't exist\n" $(basename "${dotfile_path}")
+        printf "      ${__job_task_step_symbol} No need to backup\n"
     fi
 }
 
 create_symlink() {
     local file_path="${1}"
     local link_path="${2}"
-    printf "  ${__action_step} Creating symlink\n"
-    printf "    ${__action_step} from \t%s\n" "${file_path}" 
-    printf "    ${__action_step} to \t%s\n" "${link_path}"
+    printf "  ${__job_task_step_symbol} Creating symlink\n"
+    printf "    ${__job_task_step_symbol} from \t%s\n" "${file_path}" 
+    printf "    ${__job_task_step_symbol} to \t%s\n" "${link_path}"
     ln -s "${file_path}" "${link_path}"
 }
 
 install_dotfiles() {
-    printf "${__head_action} Installing dotfiles\n"
+    printf "${__job_symbol} Installing dotfiles\n"
 
     mkdir -p "${__dotfiles_backup_dir}"
 
     for dotfile in "${!__dotfiles[@]}"; do
         print_seperator
-        printf "${__add_action} Adding %s\n" $(basename "${dotfile}")
+        printf "${__job_task_symbol} Adding %s\n" $(basename "${dotfile}")
         backup_dotfile "${HOME}/${__dotfiles[$dotfile]}"
         create_symlink "${__script_dir}/${dotfile}" "${HOME}/${__dotfiles[$dotfile]}"
     done
 
-    printf "\n${__action_completed} Dotfiles was installed \\o/\n"
+    printf "\n${__job_completed} Dotfiles was installed \\o/\n"
     print_seperator
 }
 
 install_vim_plugins() {
     local vundle_path="${HOME}/.vim/bundle/Vundle.vim"
-    printf "${__head_action} Installing Vim plugins\n"
+    printf "${__job_symbol} Installing Vim plugins\n"
     print_seperator
-    printf "${__add_action} Install Vim plugins listed in .vimrc with Vundle\n"
+    printf "${__job_task_symbol} Install Vim plugins listed in .vimrc with Vundle\n"
 
-    printf "  ${__action_step} Download Vundle to %s\n" "${vundle_path}"
+    printf "  ${__job_task_step_symbol} Download Vundle to %s\n" "${vundle_path}"
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-    printf "  ${__action_step} Installing plugins listed in ~/.vimrc with Vundle\n"
+    printf "  ${__job_task_step_symbol} Installing plugins listed in ~/.vimrc with Vundle\n"
     vim +PluginInstall +qall
 
-    printf "\n${__action_completed} Vim plugins was installed \\o/\n"
+    printf "\n${__job_completed} Vim plugins was installed \\o/\n"
     print_seperator
 }
 
@@ -148,7 +148,7 @@ main() {
     check_if_home_is_set
     install_dotfiles
     install_vim_plugins
-    printf "${__action_completed} Installation done \\o/\\o/\\o/\n"
+    printf "${__job_completed} Installation done \\o/\\o/\\o/\n"
 }
 
 main "${@}"
